22d65dc078eb6691cd5f93b33edf0a81965448f0
Pratham2324 <prathammandhar52@gmail.com>
2025-02-10 18:53:05 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -1,45 +1,21 @@
 #include <stdio.h>
 
 int main() {
-    float num1, num2, result;
-    char operator;
-
-    // Take input from the user
-    printf("Enter the first number: ");
-    scanf("%f", &num1);
-    
-    printf("Enter an operator (+, -, *, /): ");
-    scanf(" %c", &operator);  // Space before %c is used to avoid the newline character
-    
-    printf("Enter the second number: ");
-    scanf("%f", &num2);
-
-    // Perform the calculation based on the operator
-    switch (operator) {
-        case '+':
-            result = num1 + num2;
-            printf("Result: %.2f\n", result);
-            break;
-        case '-':
-            result = num1 - num2;
-            printf("Result: %.2f\n", result);
-            break;
-        case '*':
-            result = num1 * num2;
-            printf("Result: %.2f\n", result);
-            break;
-        case '/':
-            if (num2 != 0) {
-                result = num1 / num2;
-                printf("Result: %.2f\n", result);
-            } else {
-                printf("Error! Division by zero.\n");
-            }
-            break;
-        default:
-            printf("Invalid operator!\n");
-            break;
-    }
-
+    int num1,num2;
+    char sign;
+    scanf("%d %d",&num1,&num2)
+    scanf("%c",&sign);
+    if(sign = '+'){
+    printf("%d",num1+num2);
+}
+else if(sign = '-'){
+    printf("%d",num1-num2);
+}
+else if(sign = '*'){
+    printf("%d",num1*num2);
+}
+else if(sign = '/'){
+    printf("%d"num1/num2);
+}
     return 0;
 }
