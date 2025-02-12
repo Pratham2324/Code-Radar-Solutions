ff579588ae6fd60494219bb7aa0a5c731ae85c31
Pratham2324 <prathammandhar52@gmail.com>
2025-02-12 11:49:33 +0530

    Solution to Check Character Type

---
+++ Check Character Type@@6788dd63490637cfc44e1b95/code.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<type.h>
+int main(){
+    char alphabet;
+    if(alphabet == 'A' || alphabet == 'E' || alphabet == 'I' || alphabet == 'O' ||alphabet == 'U' || alphabet == 'a' || alphabet == 'e' || alphabet == 'i' || alphabet == 'o' || alphabet == 'u'){
+       printf("Vowel");
+    }
+    else{
+        printf("Consonant");
+    }
+    if(alphabet >= 0 || alphabet <= 9){
+        printf("Digit");
+    }
+    else{
+        printf("Special Character");
+    }
+    return 0;
+}
