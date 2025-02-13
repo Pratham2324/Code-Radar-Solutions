63a9dde6930a09bca44365c5db9258849ba98f11
Pratham2324 <prathammandhar52@gmail.com>
2025-02-13 13:02:52 +0530

    Solution to Check Triangle Type

--- Check Triangle Type@@6788dd63490637cfc44e1b97/code.c
+++ Check Triangle Type@@6788dd63490637cfc44e1b97/code.c
@@ -1,14 +1,11 @@
-#include <stdio.h>  // Include the standard input/output header file.
+#include <stdio.h>  
 
 int main()
 {
     int sidea, sideb, sidec; 
-    printf("Input three sides of triangle: ");  
-    scanf("%d %d %d", &sidea, &sideb, &sidec);  // Read and store the sides of the triangle.
-    // Check if the sides form a valid triangle
+    scanf("%d %d %d", &sidea, &sideb, &sidec);  
     if ((sidea + sideb > sidec) && (sidea + sidec > sideb) && (sideb + sidec > sidea)) {
-        // If the sides form a valid triangle, continue with the classification
-        if(sidea==sideb && sideb==sidec) // Check if all sides are equal.
+        if(sidea==sideb && sideb==sidec) 
         {
             printf("Equilateral");  
         }
