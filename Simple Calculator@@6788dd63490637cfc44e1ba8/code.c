4bbc80aa3cdaa48edfd6dd180a6176068a83a66e
Pratham2324 <prathammandhar52@gmail.com>
2025-02-13 12:38:10 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -16,7 +16,7 @@ int main() {
         printf("%d",num1*num2);
         break;
         case'/':
-        if(num2!= 0){
+        if(num1!= 0){
             printf("%d",num1/num2);
         }
         else{
